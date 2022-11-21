68e5fcabb3c5f955bf80259ea020eea5509a9aed
David Lee <yanwei.li.24@gmail.com>
2022-11-21 22:57:36 +0800

    generate

--- generate.sh
+++ generate.sh
@@ -10,8 +10,10 @@ block () {
 export -f block
 
 while read -r line; do
-    echo "$line" | grep '0.0.0.0' | awk '{ print $NF}' | xargs -I % bash -c 'block %'
+    echo "$line" | grep '0.0.0.0' | awk '{ print $NF}' | xargs -I % bash -c 'block % &'
 done < ./Anti-Porn-HOSTS-File/HOSTS.txt
 
+grep ':' ips | grep -v '^0.0.0.' | sort | uniq > ipv6s
+grep -v ':' ips | grep -v  ^::1$ | sort | uniq > ipv4s
 
-
+rm -rf ips
