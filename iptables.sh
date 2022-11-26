aa5bd8652f9ed1037f592a00379e667d5fbb5051
David Lee <yanwei.li.24@gmail.com>
2022-11-26 16:12:42 +0800

    tidy domain

--- iptables.sh
+++ iptables.sh
@@ -4,9 +4,11 @@ iptables -X ANTI-PORN
 
 iptables -N ANTI-PORN
 
+echo > /tmp/ips
+
 block () {
   echo block $1
-  host $1 | grep ' has address ' | awk -F ' address ' '{ print $2 }' | xargs -I % bash -c 'iptables -A ANTI-PORN -d % -j DROP'
+  host $1 | grep ' has address ' | awk -F ' address ' '{ print $2 }' | xargs -I % bash -c 'echo % >> /tmp/ips '
 }
 
 curl https://raw.githubusercontent.com/jiemoshu/anti-porn-ips/master/domains.txt > /tmp/domains.txt
@@ -14,6 +16,8 @@ curl https://raw.githubusercontent.com/jiemoshu/anti-porn-ips/master/domains.txt
 while read -r line; do
   block $line &
 done < /tmp/domains.txt
+
+cat /tmp/ips | sort | uniq | xargs -I % iptables -A ANTI-PORN -d % -j DROP
 
 iptables -I FORWARD 1 -j ANTI-PORN
 
